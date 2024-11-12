173fadca7412c172024f99ee1702b2c75570f98d
Federico Sella <fedesella95@gmail.com>
2024-11-12 12:24:26 +0100

    upp

---
+++ while.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main (){
+    int n=5;
+    int fattoriale=1;
+    while(n>0){
+        fattoriale=fattoriale*n;
+        n--;
+    }
+    printf("\nFattoriale= %d",fattoriale);
+    char c='a';
+    while(c!='v'){
+        printf("\npremi v: ");
+        scanf("%c",&c);
+        if(c!='v'){
+            printf("\nHo detto v!!!");
+        }
+    }
+    return 0;
+}
